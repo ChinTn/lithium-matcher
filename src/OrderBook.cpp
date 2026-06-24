51ca8b5a1bb0b4c49c9676fd2265fff8d425a06a
ChinTn <vaghamshichintan9@gmail.com>
2026-06-25 02:09:53 +0530

    add OrderBook

---
+++ src/OrderBook.cpp
@@ -0,0 +1,149 @@
+#include "OrderBook.h"
+#include <algorithm>
+
+// 1. Constructor: Setup the pool and trackers
+// We initialise the memory pool with 1,000,000 slots right off the bat.
+OrderBook::OrderBook() : orderPool(1000000) {
+    // we start the best bid at 0 (lowest possible)
+    // we start ask at MAX_PRICE (highest possible)
+
+    currentBestBid = 0;
+    currentBestAsk = MAX_PRICE;
+    //putting them on their worst possible state
+}
+
+// 2. Entry Point
+void OrderBook::processOrder(uint64_t orderId, Side side, uint32_t price, uint32_t quantity) {
+    
+    //Safety check
+    //Dont allow prices higher than our array size
+    if(price >= MAX_PRICE) return;
+
+    //Grab a blank order from the memory pool and fill it
+    Order* incoming = orderPool.allocate();
+    incoming->orderId = orderId;
+    incoming->side = side;
+    incoming->price = price;
+    incoming->quantity = quantity;
+    incoming->next = nullptr;
+    incoming->prev = nullptr;
+
+    // Try to match it against the opposite 
+    matchOrder(incoming);
+    
+    //If after doing this matching and trading if we still got quantity
+    //put them in List again to wait
+    if(incoming -> quantity > 0){
+        if(side == Side::BUY) {
+            addOrderToLevel(bids[price] , incoming);
+            //update the tracker if this is a new highest buyer
+            if(price > currentBestBid){
+                currentBestBid = price;
+            }
+        } else {
+            addOrderToLevel(asks[price] , incoming);
+            //update the tracker if this is a new lowest seller
+            if(price < currentBestAsk){
+                currentBestAsk = price;
+            }
+        }
+    } else {
+        
+        //means it was complete
+        orderPool.deallocate(incoming);
+    }
+}
+
+// 3. The Matching Engine Loop
+void OrderBook::matchOrder(Order* incoming) {
+    if(incoming->side == Side::BUY){
+        // While we still want to buy , AND there is a seller cheaper or equal to our price
+        // Golden Rule : Keep looping as long as Alice wants share and there is a seller willing to sell for a price lesser or equal to ALice's maximum..
+        while(incoming -> quantity > 0 && incoming -> price >= currentBestAsk){
+            PriceLevel& bestLevel = asks[currentBestAsk];
+            //here we must have to use & for referencing else later when we remove it wont get removed from the asks list
+            Order* seller = bestLevel.firstInLine;
+
+            // Find how many shares we can actually trade
+            uint32_t tradeQty = std::min(incoming->quantity , seller->quantity);
+
+            incoming -> quantity -= tradeQty;
+            seller -> quantity -= tradeQty;
+
+            // If seller is Out of shares
+            if (seller -> quantity == 0){
+                removeOrderFromLevel(bestLevel , seller);
+                //we used & up there to do this
+                orderPool.deallocate(seller); //Recycle the Order
+
+                //Also now if that Price level is complety empty, find the next best seller
+                if(bestLevel.firstInLine == nullptr) {
+                    currentBestAsk ++;
+                    //keep increasing until find the level with someone with in it
+                    while (currentBestAsk < MAX_PRICE && asks[currentBestAsk].firstInLine == nullptr){
+                        currentBestAsk ++;
+                    }
+                }
+            }
+        }
+    } else {
+
+        // While we still want to sell , and there is a buyer who is paying equal or higher than that !
+        while (incoming -> quantity > 0 && incoming -> price <= currentBestBid) {
+            PriceLevel& bestLevel = bids[currentBestBid];
+            Order* buyer = bestLevel.firstInLine;
+
+            uint32_t tradeQty = std::min(incoming -> quantity , buyer -> quantity);
+
+            incoming -> quantity -= tradeQty;
+            buyer -> quantity -= tradeQty;
+
+            //If buyer is out of Shares and we still want to share
+            if(buyer -> quantity == 0){
+                removeOrderFromLevel(bestLevel, buyer);
+                orderPool.deallocate(buyer); // Recycle the Order
+
+                //If that price of bids is now empty 
+                if(bestLevel.firstInLine == nullptr){
+                    if(currentBestBid > 0){
+                        currentBestBid--;
+                        //Keep checking
+                        while (currentBestBid > 0 && bids[currentBestBid].firstInLine == nullptr){
+                            currentBestBid--;
+                        }
+                    }
+                } 
+            }
+
+        }
+    }
+}
+
+// 4. LinkedList Mechanics (Putting someone at the back of the line)
+void OrderBook::addOrderToLevel(PriceLevel& level, Order* order) {
+    if(level.firstInLine == nullptr) {
+        //Line is empty , it is the first and last person
+        level.firstInLine = order;
+        level.lastInLine = order;
+    } else {
+        //Someone is already there
+        level.lastInLine -> next = order;
+        order -> prev = level.lastInLine;
+        level.lastInLine = order;
+    }
+}
+
+// 5. LinkedList Mechanics (Removing someone from the line)
+void OrderBook::removeOrderFromLevel(PriceLevel& level, Order* order) {
+    if(order -> prev != nullptr) {
+        order -> prev -> next = order -> next;
+    } else {
+        level.firstInLine = order -> next;
+    }
+
+    if(order -> next != nullptr) {
+        order -> next -> prev = order -> prev;
+    } else {
+        level.lastInLine = order -> prev;
+    }
+}
---
+++ src/OrderBook.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Types.h"
+#include "MemoryPool.h"
+#include <array>
+
+// We define a maximum price to size our arrays. 
+// 100,000 cents = $1,000.00
+constexpr uint32_t MAX_PRICE = 100000;
+
+class OrderBook {
+    private:
+        // 1. The Storage (The Parking Lot)
+        // We instantiate our MemoryPool specifically to hold 'Order' objects.
+        // We will initialize it with 1,000,000 slots in the .cpp file.
+        MemoryPool<Order> orderPool;
+
+        // 2. The Organizers (The Whiteboards)
+        // Arrays representing every possible price point up to $1,000.00.
+        std::array<PriceLevel, MAX_PRICE> bids; // Buyers (Highest Price is the Best)
+        std::array<PriceLevel, MAX_PRICE> asks; // Sellers (Lowest price is the Best)
+
+        // 3. The Trackers (So we don't have to search the arrays)
+        // These hold the exact array index of the current best prices.
+        uint32_t currentBestBid;
+        uint32_t currentBestAsk;
+
+        // --- Helper Functions ---
+        // These handle the annoying pointer logic of adding/removing people from the line
+
+        void addOrderToLevel(PriceLevel& level, Order* order);
+        void removeOrderFromLevel(PriceLevel& level, Order* order);
+
+        //Core Matching Logic
+        void matchOrder(Order* incomingOrder);
+    
+    public:
+        //constructor
+        OrderBook();
+
+        //The main entry point for the outside world
+        // The Mailroom thread calls this function to give us a new order.
+        void processOrder(uint64_t orderId, Side side, uint32_t price, uint32_t quantity);
+};
